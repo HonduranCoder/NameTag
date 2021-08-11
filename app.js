29bae89b62082a8b6806736859e3af3dfb0ecfbc
Estefani <stef45689@gmail.com>
2021-08-11 17:54:32 -0500

    JS added

---
+++ app.js
@@ -0,0 +1,16 @@
+const buttonEl = document.getElementById("Name-Button");
+const headerEl = document.getElementById("header"); 
+const clicksEl = document.getElementById("clickNumber");
+
+buttonEl.addEventListener('click', ()=> {
+    const inputEl = document.getElementById("input"); 
+    console.log(inputEl.value);
+
+    headerEl.textContent = inputEl.value;
+});
+
+/*let clicks = 0; 
+function onClick(){
+    clicks += 1; 
+    document.getElementById("clickNumber").innerHTML = clicks;
+};*/
